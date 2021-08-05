84b4cd4e6e3aca2ef60284d5248e3840572d8821
Ross Tomsic <ross.tomsic@gmail.com>
2021-08-05 20:09:07 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,10 +1,7 @@
-resource "azurerm_resource_group" "test" {
-  name     = "my-resources"
-  location = "EAST US"
-}
-
 module "network" {
-  source              = "Azure/network/azurerm"
-  version             = "3.0.1"
-  resource_group_name = azurerm_resource_group.test.name
+  source  = "app.terraform.io/tahitianexpress/network/azurerm"
+  version = "3.0.1"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
