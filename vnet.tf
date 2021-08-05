743b97d07841bde193f5c159e16f15a0193eb1fe
Ross Tomsic <ross.tomsic@gmail.com>
2021-08-05 19:28:39 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/tahitianexpress/network/azurerm"
   version = "3.0.1"
-  resource_group_name = myresourcegroup
+  resource_group_name = "myresourcegroup"
 
 }
