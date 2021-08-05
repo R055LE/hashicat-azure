94633a1bb30316ba9ed90abcbf2861338f048d50
Ross Tomsic <ross.tomsic@gmail.com>
2021-08-05 19:15:17 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,8 @@
 module "network" {
   source  = "app.terraform.io/tahitianexpress/network/azurerm"
   version = "3.0.1"
-  # insert required variables here
+  resource_group_name = azurerm_resource_group.test.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 }
